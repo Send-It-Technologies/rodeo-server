e94b53bacccce79a3e5f47ab7e4cc62e99e9542f
Krishang <krishang@thirdweb.com>
2025-02-19 09:25:38 +0530

    debug relay/chain

--- src/routes/relay/index.ts
+++ src/routes/relay/index.ts
@@ -61,6 +61,7 @@ export function relayRoutes(): Hono<{ Bindings: Env }> {
       }
 
       // Send transaction to engine relayer and get queue Id
+      console.log("BASE chainId: ", base.id.toString());
       const queueId = await relay({
         to,
         data,
@@ -122,6 +123,7 @@ export function relayRoutes(): Hono<{ Bindings: Env }> {
       }
 
       // Send transaction to engine relayer and get queue Id
+      console.log("Params chainId: ", chainId);
       const queueId = await relay({
         to,
         data,
